87f9681e7def5ec664a996f46b22e4478f200fb5
cj <Jungreithmair.Christof@gmx.at>
2022-06-05 14:37:50 +0000

    Unterscheidung (daszweite)

---
+++ pages/_app.js
@@ -0,0 +1,36 @@
+import '../css/style.css'
+import '../css/form.css'
+import Head from 'next/head'
+import Link from 'next/link'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Pet Care App</title>
+      </Head>
+
+      <div className="top-bar">
+        <div className="nav">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/new">
+            <a>Add Pet</a>
+          </Link>
+        </div>
+
+        <img
+          id="title"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo!_Logo_2021.svg/512px-ZDF_logo!_Logo_2021.svg.png"
+          alt="logo for WEBA"
+        ></img>
+      </div>
+      <div className="grid wrapper">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
+}
+
+export default MyApp
